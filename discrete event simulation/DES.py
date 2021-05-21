0da4c4724711fba7070da7fe1ac66d1a4c757145
MunyaradziMagura <43807813+MunyaradziMagura@users.noreply.github.com>
2021-05-22 02:13:38 +0930

    vibration limiter added

--- discrete event simulation/DES.py
+++ discrete event simulation/DES.py
@@ -52,18 +52,20 @@ def sensor_generator(env, sensor_id,start_temperature,highest_temp, start_vibrat
         
         while True:
             
-            # print("highest temp = ", highest_temp)
+            # random number generator
             interarrival = random.expovariate(sensor_id + 1)
-
+            # simulate vibration spikes 
             vibration = secrets.randbelow(start_vibration)
+            # ensure the vibration is always above 0
             while vibration == 0:
                 vibration = secrets.randbelow(start_vibration)
-
+            #  send warning if the tempreture is too hot or vibration is too much
             if temperature >= warning_temp[1] and temperature < warning_temp[2]: 
                 print("WARNING PIPE IS WARMING UP")
             if temperature >= warning_temp[2]: 
                 print("COLD-RED PIPE IS HOT!!!")
-            
+            if vibration > limit_vibration:
+                print(f"WARNING VIBRATION LIMIT REACHED. PIPE INTEGRETTY AT RISK.VIBRATION: {vibration}")
             # if the temp reaches the pipe limit then we simulate the user manually lowering it
             if temperature > limit_temperature:
                 print("pipe temperture limit reached")
@@ -92,7 +94,7 @@ start_temperature = 24
 warning_temp = [start_temperature, 38, 39]
 start_vibration = 10
 limit_temperature = 40
-limit_vibration = 40
+limit_vibration = 1
 sensor_interval_time = 20
 
 env.process(pipe_generator(env, start_temperature, start_vibration, limit_temperature, limit_vibration, sensor_interval_time, sensor, warning_temp))
