5fe2bdfa0acce7cc633ab630be22afbc30d5dc70
MunyaradziMagura <43807813+MunyaradziMagura@users.noreply.github.com>
2021-05-28 20:50:08 +0930

    Merge branch 'main' of https://github.com/MunyaradziMagura/IT-project into main

--- discrete event simulation/DES.py
+++ discrete event simulation/DES.py
@@ -1,5 +1,6 @@
 import simpy
 import random
+import secrets
 
 #  generate the pipe 
 def pipe_generator(env, start_temperature, start_vibration, limit_temperature, limit_vibration, sensor_interval_time, sensor, warning_temp):
@@ -39,34 +40,41 @@ def sensor_generator(env, sensor_id,start_temperature,highest_temp, start_vibrat
     # record the time the water starts changing tempreture at this sensor
     current_time = env.now
     print("Sensor:",sensor_id," started changing tempretures at",current_time," its current tempreture is ",start_temperature)
-    #  this will be the tempreture varible 
+    # temperature 
     temperature = start_temperature
+    # vibration 
+
     # request the sensor data
     with sensor.request() as req:
         # stop if no sensor is found 
         yield req
-
-       
         con_time = random.expovariate(1.0 / sensor_interval_time)
         
         while True:
-
-            # print("highest temp = ", highest_temp)
+            
+            # random number generator
             interarrival = random.expovariate(sensor_id + 1)
+            # simulate vibration spikes 
+            vibration = secrets.randbelow(start_vibration)
+            
+            # ensure the vibration is always above 0
+            while vibration == 0:
+                vibration = secrets.randbelow(start_vibration)
 
+            #  send warning if the tempreture is too hot or vibration is too much
             if temperature >= warning_temp[1] and temperature < warning_temp[2]: 
                 print("WARNING PIPE IS WARMING UP")
             if temperature >= warning_temp[2]: 
                 print("COLD-RED PIPE IS HOT!!!")
-            
+            if vibration > limit_vibration:
+                print(f"WARNING VIBRATION LIMIT REACHED. PIPE INTEGRETTY AT RISK.VIBRATION: {vibration}")
             # if the temp reaches the pipe limit then we simulate the user manually lowering it
             if temperature > limit_temperature:
                 print("pipe temperture limit reached")
                 temperature -= random.expovariate(1.0/interarrival) + (highest_temp / 3)
             else:
                 temperature += random.expovariate(1.0/interarrival)
-                
-            
+
             # get the final tempreture 
             highest_temp = temperature
             
@@ -74,15 +82,11 @@ def sensor_generator(env, sensor_id,start_temperature,highest_temp, start_vibrat
             yield env.timeout(interarrival,highest_temp)
 
             # to make things interesting, we add some printouts
-            print( f'temp changed to {temperature:5.2f} Celsius at {env.now:5.2f} min/s')
-        
-        yield env.timeout(con_time, highest_temp)
-        
+            print( f'temp changed to {temperature:5.2f} degree/s Celsius at {env.now:5.2f} min/s || vibrated by {vibration} cubic meters')
 
 
-
-    # how fast it travles in cubic meters
-
+# notifications need to be in diffenrt tables
+# data can be generated in sec or min
 
 # initlise the descrete event simulation enviroment
 env = simpy.Environment()
@@ -92,9 +96,9 @@ sensor = simpy.Resource(env, capacity=1)
 start_temperature = 24
 # [good, warning, cold-red]
 warning_temp = [start_temperature, 38, 39]
-start_vibration = 2
+start_vibration = 10
 limit_temperature = 40
-limit_vibration = 40
+limit_vibration = 1
 sensor_interval_time = 20
 
 env.process(pipe_generator(env, start_temperature, start_vibration, limit_temperature, limit_vibration, sensor_interval_time, sensor, warning_temp))
