ebbc392ee29f392057115cb0ce2008229edc228e
MunyaradziMagura <43807813+MunyaradziMagura@users.noreply.github.com>
2021-05-30 17:14:52 +0930

    csv output complete

--- discrete event simulation/DES.py
+++ discrete event simulation/DES.py
@@ -79,7 +79,6 @@ def sensor_generator(env, sensor_id,start_temperature,highest_temp, start_vibrat
     # temperature 
     temperature = start_temperature
    
-    # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   ",sensor_id)
     new_id = int(open("id.txt", "r").read())
     # request the sensor data
     with sensor.request() as req:
@@ -141,15 +140,19 @@ def sensor_generator(env, sensor_id,start_temperature,highest_temp, start_vibrat
 
             # get current time into an array
             print(f"current_time {env.now:5.2f} |" ,end="")
-            time = [f"current_time {env.now:5.2f}"]
+            time = [f"{env.now:5.2f}"]
             if new_id == 0:
                 print( f' temp changed to {temperature:5.2f} degree/s Celsius at {env.now:5.2f} min/s || vibrated by {vibration} cubic meters |', end="")
+                print("N/A   |", end="")  # sensor id = 1
+                print("N/A")  # sensor id = 2
+
                 # time, warnings
                 # set sensor one data
                 sensor_one_data = [f'{temperature:5.2f} ',f'{vib_warning}',f'{temp_alerm}',f'{temp_emergency}',f'{vibration}',f'{vib_warning}',f'{vib_alerm}',f'{vib_emergency}']
-                # [f'{}',f'{}',f'{}',f'{}',f'{}',f'{}',]
-                print("N/A   |", end="")  # sensor id = 1
-                print("N/A")  # sensor id = 2
+                sensor_two_data = [f'{start_temperature}',f'{False}',f'{False}',f'{False}',f'{start_vibration}',f'{False}',f'{False}',f'{False}']
+                sensor_three_data = [f'{start_temperature}',f'{False}',f'{False}',f'{False}',f'{start_vibration}',f'{False}',f'{False}',f'{False}']
+                # [f'{}',f'{}',f'{}',f'{}',f'{}',f'{}',f'{}',f'{}']
+                rows.append(time + sensor_one_data + sensor_two_data + sensor_three_data)
             elif new_id == 1:
                 print("N/A   |", end="")  # sensor id = 0
                 print( f' temp changed to {temperature:5.2f} degree/s Celsius at {env.now:5.2f} min/s || vibrated by {vibration} cubic meters |', end="")
